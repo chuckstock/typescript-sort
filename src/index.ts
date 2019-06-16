1201f87a264c37103c319fad432599faa33bb83d
Charlie Blackstock <charlieblackstock@gmail.com>
2019-06-16 15:39:13 -0600

    init commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+
+// const numbers = new NumbersCollection([10, 0, 3, -5, 6])
+// const sorter = new Sorter(numbers)
+// const string = new CharactersCollection('Xaysi')
+// const stringSorter = new Sorter(string)
+// const linkedList = new LinkedList()
+// linkedList.add(500)
+// linkedList.add(-10)
+// linkedList.add(3)
+// linkedList.add(12)
+// const linkedSorter = new Sorter(linkedList)
+// linkedSorter.sort()
+// linkedList.print()
+// stringSorter.sort()
+// sorter.sort()
+// console.log(numbers.data)
+// console.log(string.data)
